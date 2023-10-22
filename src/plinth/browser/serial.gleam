ae18256f9337d7c506f4f9b934a98863923bb672
Peter <peterhsaxton@gmail.com>
2023-10-22 17:26:59 +0200

    implement reading from a serial port

---
+++ src/plinth/browser/serial.gleam
@@ -0,0 +1,23 @@
+import gleam/javascript/array.{Array}
+import gleam/javascript/promise.{Promise}
+pub type SerialPort
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Serial/requestPort
+@external(javascript, "../../serial_ffi.mjs", "requestPort")
+pub fn request_port() -> Promise(Result(SerialPort, Nil))
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Serial/getPorts
+@external(javascript, "../../serial_ffi.mjs", "getPorts")
+pub fn get_ports() -> Promise(Result(List(SerialPort), Nil))
+
+// https://developer.mozilla.org/en-US/docs/Web/API/SerialPort/getInfo
+@external(javascript, "../../serial_ffi.mjs", "getInfo")
+pub fn get_info(port: SerialPort) -> Result(#(Int, Int), Nil)
+
+// https://developer.mozilla.org/en-US/docs/Web/API/SerialPort/open
+@external(javascript, "../../serial_ffi.mjs", "open")
+pub fn open(port: SerialPort, baud_rate: Int) -> Promise(Result(Nil, Nil))
+
+// https://developer.mozilla.org/en-US/docs/Web/API/SerialPort#reading_data_from_a_port
+@external(javascript, "../../serial_ffi.mjs", "read")
+pub fn read(port: SerialPort, callback: fn(BitString) -> Nil) -> Promise(Result(Nil, Nil))
---
+++ src/serial_ffi.mjs
@@ -0,0 +1,63 @@
+import { Ok, Error, BitString } from "./gleam.mjs";
+
+export async function requestPort() {
+  try {
+    const port = await globalThis.navigator.serial.requestPort({ filters: [] });
+    return new Ok(port);
+  } catch (error) {
+    console.warn(error);
+    return new Error();
+  }
+}
+
+export async function getPorts() {
+  try {
+    const ports = await globalThis.navigator.serial.getPorts();
+    return new Ok(ports);
+  } catch (error) {
+    console.warn(error);
+    return new Error();
+  }
+}
+
+export function getInfo(port) {
+  try {
+    const { usbVendorId, usbProductId } = port.getInfo();
+    return new Ok([usbVendorId, usbProductId]);
+  } catch (error) {
+    console.warn(error);
+    return new Error();
+  }
+}
+
+export async function open(port, baudRate) {
+  try {
+    await port.open({ baudRate });
+    return new Ok();
+  } catch (error) {
+    console.warn(error);
+    return new Error();
+  }
+}
+
+export async function read(port, callback) {
+  while (port.readable) {
+    const reader = port.readable.getReader();
+    try {
+      while (true) {
+        const { value, done } = await reader.read();
+        if (done) {
+          // |reader| has been canceled.
+          break;
+        }
+        callback(new BitString(value));
+        // Do something with |value|…
+      }
+      return new Ok();
+    } catch (error) {
+      return new Error();
+    } finally {
+      reader.releaseLock();
+    }
+  }
+}
