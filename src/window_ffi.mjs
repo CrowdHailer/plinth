a94d9bd7704f718a74108497058d5b5176ec25d7
Peter <peterhsaxton@gmail.com>
2024-01-05 20:50:56 +0100

    open window object

--- src/window_ffi.mjs
+++ src/window_ffi.mjs
@@ -1,3 +1,67 @@
+import { Ok, Error } from "./gleam.mjs";
+
+export function self() {
+  return window;
+}
+
 export function addEventListener(type, listener) {
   return window.addEventListener(type, listener);
+}
+
+export function location() {
+  return window.location.href;
+}
+
+export function locationOf(w) {
+  return w.location.href;
+}
+
+export function getHash() {
+  const hash = window.location.hash;
+  if (hash == "") {
+    return new Error();
+  }
+
+  return decodeURIComponent(hash.slice(1));
+}
+
+export function getSearch() {
+  const search = window.location.search;
+  if (search == "") {
+    return new Error();
+  }
+
+  return decodeURIComponent(search.slice(1));
+}
+
+export function innerHeight(w) {
+  return w.innerHeight;
+}
+
+export function innerWidth(w) {
+  return w.innerWidth;
+}
+
+export function outerHeight(w) {
+  return w.outerHeight;
+}
+
+export function outerWidth(w) {
+  return w.outerWidth;
+}
+
+export function open(url, target, features) {
+  try {
+    return new Ok(window.open(url, target, features));
+  } catch (error) {
+    return new Error(error.toString());
+  }
+}
+
+export function close(w) {
+  w.close();
+}
+
+export function closed(w) {
+  return w.closed;
 }
