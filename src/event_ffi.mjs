a0d4e46ba56fff7e17568793572710f876d0a3b3
cdaringe <cdaringe@gmail.com>
2024-06-27 13:27:07 -0700

    feat: add key_code

--- src/event_ffi.mjs
+++ src/event_ffi.mjs
@@ -6,6 +6,10 @@ export function key(event) {
   return event.key;
 }
 
+export function keyCode(event) {
+  return event.keyCode;
+}
+
 
 export function preventDefault(event) {
   return event.preventDefault;
