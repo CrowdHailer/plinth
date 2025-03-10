4b57c89d872a5d9f94570d71abdf137a7ebab957
jrstrunk <developer@jrstrunk.com>
2025-03-10 09:29:36 -0400

    add bindings for selecting elements within a shadow root

--- src/shadow_ffi.mjs
+++ src/shadow_ffi.mjs
@@ -1,3 +1,4 @@
+import { Ok, Error } from "./gleam.mjs";
 import { Open } from "./plinth/browser/shadow.mjs"
 
 export function appendChild(root, element) {
@@ -7,4 +8,24 @@ export function appendChild(root, element) {
 export function attachShadow(element, mode) {
   const shadowMode = mode instanceof Open ? "open" : "closed"
   return element.attachShadow({ mode: shadowMode })
+}
+
+export function shadowRoot(element) {
+  let shadowRoot = element.shadowRoot;
+  if (shadowRoot) {
+    return new Ok(shadowRoot);
+  }
+  return new Error();
+}
+
+export function querySelector(shadowRoot, selector) {
+  let element = shadowRoot.querySelector(selector);
+  if (element) {
+    return new Ok(element);
+  }
+  return new Error();
+}
+
+export function querySelectorAll(shadowRoot, selector) {
+  return shadowRoot.querySelectorAll(selector);
 }
