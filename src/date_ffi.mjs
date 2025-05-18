2dccb445faf535ab811f0170d034551d90a91b4a
graphiteisaac <mrminer2229.galaxycraft@gmail.com>
2025-05-18 13:29:27 +1000

    switch from deprecated Date.getYear() to date.getFullYear()

--- src/date_ffi.mjs
+++ src/date_ffi.mjs
@@ -1,39 +1,39 @@
 export function now() {
-  return new Date();
+	return new Date();
 }
 
 export function new_(string) {
-  return new Date(string);
+	return new Date(string);
 }
 
 export function toISOString(d) {
-  return d.toISOString();
+	return d.toISOString();
 }
 
 export function year(d) {
-  return d.getYear();
+	return d.getFullYear();
 }
 
 export function month(d) {
-  return d.getMonth();
+	return d.getMonth();
 }
 
 export function day(d) {
-  return d.getDay();
+	return d.getDay();
 }
 
 export function date(d) {
-  return d.getDate();
+	return d.getDate();
 }
 
 export function hours(d) {
-  return d.getHours();
+	return d.getHours();
 }
 
 export function minutes(d) {
-  return d.getMinutes();
+	return d.getMinutes();
 }
 
 export function seconds(d) {
-  return d.getSeconds();
+	return d.getSeconds();
 }
